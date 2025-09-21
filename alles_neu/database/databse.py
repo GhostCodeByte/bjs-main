21e286e5f14427992d3ca25cd20d87ec9b89957c
Elian Darou <ghostgamingmcw@outlook.de>
2025-09-21 14:42:17 +0200

    database startet

--- alles_neu/database/databse.py
+++ alles_neu/database/databse.py
@@ -1,3 +1,20 @@
 import sqlite3
+from datetime import datetime
 import os
 
+class Database:
+    # Die ID kann genuzt werden wen man mehrere datenbanken gleichzeitig haben will 
+    def __init__(self, id = None):
+
+        if id == None:
+            id = datetime.now().year
+
+        self.connection = sqlite3.connect(f"bjs_database_{id}.db")
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        if not self.cursor.fetchall():
+            self.datenbank_erstellen()
+
+    def datenbank_erstellen():
+        pass
